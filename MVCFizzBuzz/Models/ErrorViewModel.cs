07a3affd3ea85534993eebcffc682a7ae08ae4cc
Lyso95 <amos@langensiepen.net>
2021-12-17 20:55:14 +0100

    Add project files.

---
+++ MVCFizzBuzz/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCFizzBuzz.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
